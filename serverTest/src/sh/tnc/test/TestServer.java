41288d0b9db38194ecc6a2c88b05cf8eace21c63
doto <doto@thenetcircle.com>
2016-03-08 15:39:51 +0800

    just added new files

---
+++ serverTest/src/sh/tnc/test/TestServer.java
@@ -0,0 +1,52 @@
+package sh.tnc.test;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class TestServer
+ */
+@WebServlet("/TestServer")
+public class TestServer extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public TestServer() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+		response.setContentType("text");
+		PrintWriter out = response.getWriter();
+		out.println("<html>");
+		out.println("<head><title> Hello</title></head>");
+		out.println("<body>");
+		out.println("<h1>");
+		out.println("Here we are");
+		out.println("</h1>");
+		out.println("</body>");
+		out.println("</html>");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
